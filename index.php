e94b72e708ae37be2b144bc489bf9bf0ff1236df
Divyansh Rathore <54471929+divyanshrathore09@users.noreply.github.com>
2022-10-31 22:31:18 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Divyansh - CodePipeline</title>
+</head>
+<body>
+        <h1><?php echo 'Hello, Divyansh!'; ?></h1>
+</body>
+</html>
